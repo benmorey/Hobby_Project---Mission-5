0d37f15a54e3e20a88215f1e495680f0e1e63106
benmorey <dbmoreyjr@gmail.com>
2024-02-09 16:42:11 -0700

    Add project files.

---
+++ Hobby_Project - Mission 5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hobby_Project___Mission_5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Hobby_Project - Mission 5/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(function () {
+    $('#calculateButton').click(function () {
+        var hours = parseFloat($('#hours').val());
+        var tip = parseFloat($('#tip').val());
+
+        if (isNaN(hours)) {
+            $('#totalCost').text("Please enter valid numbers");
+        } else {
+            var totalCost = (hours * 20.50) + tip;
+            $('#totalCost').text('$' + totalCost.toFixed(2));
+        }
+    });
+});
